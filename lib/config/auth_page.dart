539649d43c757cf69da2533db7da6444f724cf83
Srivatsan <srivatsanb123@gmail.com>
2023-12-28 05:16:29 +0530

    Fix Null Error

--- lib/config/auth_page.dart
+++ lib/config/auth_page.dart
@@ -2,15 +2,30 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
-  signInWithGoogle() async {
-    final GoogleSignIn _googleSignIn = GoogleSignIn(
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      final GoogleSignIn _googleSignIn = GoogleSignIn(
         clientId:
-            '191251944607-1fnl9eeloo9og88lb22apubrc8nt3ofe.apps.googleusercontent.com');
-    final GoogleSignInAccount? gUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
-    final credential = GoogleAuthProvider.credential(
-        accessToken: gAuth.accessToken, idToken: gAuth.idToken);
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+            '191251944607-1fnl9eeloo9og88lb22apubrc8nt3ofe.apps.googleusercontent.com',
+      );
+
+      final GoogleSignInAccount? gUser = await _googleSignIn.signIn();
+
+      if (gUser == null) {
+        // User canceled the sign-in
+        return null;
+      }
+
+      final GoogleSignInAuthentication gAuth = await gUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: gAuth.accessToken,
+        idToken: gAuth.idToken,
+      );
+
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      print('Error during Google sign-in: $e');
+      return null;
+    }
   }
 }
-
